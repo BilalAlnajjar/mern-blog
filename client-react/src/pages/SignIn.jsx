131b6c608c94df1cd8bf64884249f96023ea07aa
BilalAlnajjar <bilalalnajjar46@gmail.com>
2024-09-26 15:10:50 +0300

    add redux toolkit

--- client-react/src/pages/SignIn.jsx
+++ client-react/src/pages/SignIn.jsx
@@ -1,12 +1,15 @@
 import React, {useState} from 'react';
 import {Link,useNavigate} from "react-router-dom";
 import {Alert, Button, Label, Spinner, TextInput} from "flowbite-react";
+import {useDispatch,useSelector} from "react-redux";
+import {signInStart,signInSuccess,signInFailure} from "../redux/user/userSlice.js";
 
 function SignIn(props) {
     const [formData,setFormData] = useState({})
-    const [errorMessage,setErrorMessage] = useState(null)
-    const [loading,setLoading] = useState(false)
+    const { loading, error:errorMessage } = useSelector(state => state.user)
     const navigate = useNavigate()
+    const dispatch = useDispatch()
+
 
     const handelChange = (e) => {
         setFormData({...formData, [e.target.id]: e.target.value.trim()})
@@ -14,11 +17,10 @@ function SignIn(props) {
     const handelSubmit = async (e) => {
         e.preventDefault()
         if (!formData.email || !formData.password){
-            return setErrorMessage("Please fill out all fields")
+            return dispatch(signInFailure("Please fill out all fields"))
         }
         try{
-            setLoading(true)
-            setErrorMessage(null)
+            dispatch(signInStart())
             const res = await fetch('/api/auth/signin',{
                 method:"POST",
                 headers:{'Content-Type':'application/json'},
@@ -26,16 +28,15 @@ function SignIn(props) {
             })
 
             const data = await res.json()
-            setLoading(false)
             if(data.success === false){
-                return setErrorMessage(data.message)
+                dispatch(signInFailure(data.message))
             }
             if(res.ok){
+                dispatch(signInSuccess(data))
                 navigate("/")
             }
         }catch (error){
-            setErrorMessage(error.message)
-            setLoading(false)
+            dispatch(signInFailure(error.message))
         }
     }
     console.log(formData)
