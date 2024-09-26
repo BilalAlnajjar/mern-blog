7882acb4d48cfe7da5cbc1c19f5cbd56b4f6d7f4
BilalAlnajjar <bilalalnajjar46@gmail.com>
2024-09-26 10:13:07 +0300

    add functionality to sign up page

--- client-react/src/pages/SignUp.jsx
+++ client-react/src/pages/SignUp.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
-import {Link} from "react-router-dom";
-import {Button, Label, TextInput} from "flowbite-react";
+import React, {useState} from 'react';
+import {Link,useNavigate} from "react-router-dom";
+import {Alert, Button, Label, Spinner, TextInput} from "flowbite-react";
 
 function SignUp(props) {
+    const [formData,setFormData] = useState({})
+    const [errorMessage,setErrorMessage] = useState(null)
+    const [loading,setLoading] = useState(false)
+    const navigate = useNavigate()
+
+    const handelChange = (e) => {
+        setFormData({...formData, [e.target.id]: e.target.value.trim()})
+    }
+    const handelSubmit = async (e) => {
+        e.preventDefault()
+        if (!formData.username || !formData.email || !formData.password){
+            return setErrorMessage("Please fill out all fields")
+        }
+        try{
+            setLoading(true)
+            setErrorMessage(null)
+            const res = await fetch('/api/auth/signup',{
+                method:"POST",
+                headers:{'Content-Type':'application/json'},
+                body:JSON.stringify(formData)
+            })
+
+            const data = await res.json()
+            setLoading(false)
+            if(data.success === false){
+                return setErrorMessage(data.message)
+            }
+            if(res.ok){
+                navigate("/sign-in")
+            }
+        }catch (error){
+            setErrorMessage(error.message)
+            setLoading(false)
+        }
+    }
+    console.log(formData)
     return (
         <div className={"min-h-screen mt-20"}>
             <div className={"flex p-3 max-w-3xl mx-auto flex-col " +
@@ -20,27 +56,38 @@ function SignUp(props) {
                 </div>
                 {/*right side*/}
                 <div className={"flex-1"}>
-                    <form className={"flex flex-col gap-4"}>
+                    <form className={"flex flex-col gap-4"} onSubmit={handelSubmit}>
                         <div className={""}>
                             <Label value={"Your username"}/>
-                            <TextInput type={"text"} placeholder={"Username"} id={"username"}/>
+                            <TextInput type={"text"} placeholder={"Username"} id={"username"} onChange={handelChange}/>
                         </div>
                         <div className={""}>
                             <Label value={"Your email"}/>
-                            <TextInput type={"text"} placeholder={"name@company.com"} id={"email"}/>
+                            <TextInput type={"email"} placeholder={"name@company.com"} id={"email"} onChange={handelChange}/>
                         </div>
                         <div className={""}>
                             <Label value={"Your password"}/>
-                            <TextInput type={"text"} placeholder={"Password"} id={"password"}/>
+                            <TextInput type={"password"} placeholder={"Password"} id={"password"} onChange={handelChange}/>
                         </div>
-                        <Button gradientDuoTone={"purpleToPink"}>
-                            Sign Up
+                        <Button type={"submit"} gradientDuoTone={"purpleToPink"} disabled={loading}>
+                            {
+                                loading ?<>
+                                    <Spinner size={"sm"} />
+                                    <span className={"pl-3"}>Loading...</span>
+                                </>
+                                    : "Sign Up"
+                            }
                         </Button>
                     </form>
                     <div className={"flex gap-2 text-sm mt-5"}>
                         <span>Have an a account?</span>
                         <Link to={"/sign-in"} className={"text-blue-500"}>Sign In</Link>
                     </div>
+                    {
+                        errorMessage && (
+                            <Alert className={"mt-5"} color={"failure"}>{errorMessage}</Alert>
+                        )
+                    }
                 </div>
             </div>
         </div>
--- client-react/vite.config.js
+++ client-react/vite.config.js
@@ -4,6 +4,14 @@ import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      '/api':{
+        target:'http://localhost:3000',
+        secure:false,
+      }
+    }
+  },
   plugins: [react()],
   css: {
     postcss: {
