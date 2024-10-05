37ba008c8ac82e43f52007cea897e19c1af4320d
Ahmed Haddaji <137641951+HaddajiDev@users.noreply.github.com>
2024-10-05 15:41:29 +0100

    Delete all posts associated with the user

--- api-node/controllers/user.controller.js
+++ api-node/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import {errorHandler} from "../utils/error.js";
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
+import Post from "../models/post.model.js";
 
 export const test = (req,res) => {
     res.json({message: "API is working in user controller"});
@@ -55,6 +56,10 @@ export const deleteUser = async (req,res,next) =>{
         return next(errorHandler(403,"You are not allowed to delete this user"))
     }
     try{
+        // Delete all posts associated with the user
+        await Post.deleteMany({ userId: req.params.userId });
+
+        //Delete the user
         await User.findByIdAndDelete(req.params.userId)
         res.status(200).json("User has been deleted")
     }catch (error){
