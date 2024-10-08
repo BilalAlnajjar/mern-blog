2348540a0725fa6df7d0f132517b3360ae1a867d
BilalAlnajjar <bilalalnajjar46@gmail.com>
2024-10-08 13:24:56 +0300

    create get posts api route

--- api-node/controllers/post.controller.js
+++ api-node/controllers/post.controller.js
@@ -26,3 +26,45 @@ export const createPost  = async (req,res,next) => {
       next(err)
     }
 }
+
+export const getPosts = async (req, res, next) => {
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 9;
+        const sortDirection = req.query.order === "desc" ? 1 : -1;
+
+        // البحث عن المنشورات باستخدام الفلاتر
+        const posts = await Post.find({
+            ...(req.query.userId && {userId: req.query.userId}),
+            ...(req.query.category && {category: req.query.category}),
+            ...(req.query.slug && {slug: req.query.slug}),
+            ...(req.query.postId && {_id: req.query.postId}),
+            ...(req.query.searchTerm && {
+                $or: [
+                    {title: {$regex: req.query.searchTerm, $options: "i"}},
+                    {content: {$regex: req.query.searchTerm, $options: "i"}}
+                ]
+            })
+        }).sort({ updatedAt: sortDirection }).skip(startIndex).limit(limit);
+
+        // حساب إجمالي عدد المنشورات
+        const totalPosts = await Post.countDocuments();
+
+        // حساب المنشورات التي تم إنشاؤها خلال الشهر الماضي
+        const now = new Date();
+        const oneMonthAgo = new Date(now.getFullYear(), now.getMonth() - 1, now.getDate());
+        const lastMonthPosts = await Post.countDocuments({
+            createdAt: { $gte: oneMonthAgo },
+        });
+
+        // إرسال البيانات كاستجابة
+        res.status(200).json({
+            posts,
+            totalPosts,
+            lastMonthPosts
+        });
+
+    } catch (error) {
+        next(error);
+    }
+};
