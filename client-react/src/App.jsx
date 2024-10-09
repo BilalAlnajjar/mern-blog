3a9f8693b93b541991641f8a46676e97a5b99135
BilalAlnajjar <bilalalnajjar46@gmail.com>
2024-10-09 12:41:45 +0300

    add update post functionality

--- client-react/src/App.jsx
+++ client-react/src/App.jsx
@@ -10,6 +10,7 @@ import FooterComponent from "./components/Footer.jsx";
 import PrivateRoute from "./components/PrivateRoute.jsx";
 import AdminPrivateRoute from "./components/AdminPrivateRoute.jsx";
 import CreatePost from "./pages/CreatePost.jsx";
+import UpdatePost from "./pages/UpdatePost.jsx";
 
 function App() {
     return (
@@ -25,6 +26,7 @@ function App() {
                 </Route>
                 <Route element={<AdminPrivateRoute />}>
                     <Route path="/create-post" element={<CreatePost />} />
+                    <Route path="/update-post/:postId" element={<UpdatePost />} />
                 </Route>
                 <Route path="/projects" element={<Projects />} />
             </Routes>
